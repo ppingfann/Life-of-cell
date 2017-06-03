3fa14c34d668ae08c71519f4a39d6c94e2e4c683
ppingfann <404015860@qq.com>
2017-06-03 10:51:28 +0800

    add dead

--- LifeOfCell/src/main/java/CellLife.java
+++ LifeOfCell/src/main/java/CellLife.java
@@ -4,16 +4,34 @@ import java.util.Scanner;
  * Created by Administrator on 2017/6/3.
  */
 public class CellLife {
+    private static final char life = '*';
+    private static final char dead = '-';
+
     public void inputCellOfArray(){
         Scanner scanner = new Scanner(System.in);
-        int[][] cellArray = new int[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        int n = scanner.nextInt();
+        int[][] cellArray = new int[n][n];
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-1; j++) {
                 cellArray[i][j] = scanner.nextInt();
             }
         }
+        for (int i = 0; i < n; i++) {
+            cellArray[0][i] = dead;
+            cellArray[i][0] = dead;
+            cellArray[n-1][i] = dead;
+            cellArray[i][n-1] = dead;
+        }
     }
+
+    public  int
     public int[][] trans(int[][] array){
+
+
+    }
+
+    public int neighbors(int row ,int col){
+
 
     }
 
