d0641d417ec39366e8a8281d93c485042c893cb3
ppingfann <404015860@qq.com>
2017-06-03 14:35:29 +0800

    output consloe and cir

--- LifeOfCell/src/main/java/CellLife.java
+++ LifeOfCell/src/main/java/CellLife.java
@@ -101,19 +101,20 @@ public class CellLife {
     }
 
     public void output(char[][] array) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.println(array[i][j] + " ");
+        for (int i = 1; i < 4; i++) {
+            for (int j = 1; j < 4; j++) {
+                System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
+        System.out.println();
     }
 
     public static void main(String args[]) {
         CellLife cellLife = new CellLife();
-        cellLife.inputCellOfArray();
+        cellLife.output(cellLife.inputCellOfArray());
         cellLife.circu(cellArray,2);
-        
+
 
 
 
